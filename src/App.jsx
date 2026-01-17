1ad4c56e33a5a154f39d84996140e30806d5907a
assumpta15 <assumptaozor6@gmail.com>
2026-01-17 07:09:18 +0100

    Remove admin imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import PublicProjects from "./pages/PublicProjects";
-// import ProjectDetails from "./pages/ProjectDetails";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<PublicProjects />} />
-//         <Route path="/projects" element={<PublicProjects />} />
-//         <Route path="/projects/:id" element={<ProjectDetails />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-
-
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -26,14 +5,14 @@ import Home from "./pages/Home";
 import PublicProjects from "./pages/PublicProjects";
 import ProjectDetails from "./pages/ProjectDetails";
 import Contact from "./pages/Contact";
-import AdminMessages from "../../admin/src/pages/AdminMessages";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
 
-      {/* push content down because navbar is fixed */}
+      
       <div className="pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
