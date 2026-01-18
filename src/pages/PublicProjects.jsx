6f7d97eaa6786834677e51088a39823945316b74
assumpta15 <assumptaozor6@gmail.com>
2026-01-18 14:41:53 +0100

    Fix axios import path

--- src/pages/PublicProjects.jsx
+++ src/pages/PublicProjects.jsx
@@ -1,8 +1,8 @@
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-//import api from "../utils/api";
-import api from "../api/axios";
+import api from "../utils/api";
+
 
 export default function PublicProjects() {
   const [projects, setProjects] = useState([]);
